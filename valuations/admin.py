fd675c9c6c176fb567dd292794dd467192a4630c
Lydiah-Nyawira <lydiahnyawira4@gmail.com>
2026-08-17 11:30:14 +0300

    Fix missing import for InspectionDetails in admin

--- valuations/admin.py
+++ valuations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ValuationAssignment
+from .models import ValuationAssignment, InspectionDetails
 
 admin.site.register(ValuationAssignment)
 admin.site.register(InspectionDetails)
