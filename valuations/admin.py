25893af38b3a1e7800c992f0ede3df311b2abb1f
Lydiah-Nyawira <lydiahnyawira4@gmail.com>
2026-08-17 11:36:52 +0300

    Register ValuationResult model in admin

--- valuations/admin.py
+++ valuations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ValuationAssignment, InspectionDetails
+from .models import ValuationAssignment, InspectionDetails, ValuationResult
 
 admin.site.register(ValuationAssignment)
 admin.site.register(InspectionDetails)
+admin.site.register(ValuationResult)
