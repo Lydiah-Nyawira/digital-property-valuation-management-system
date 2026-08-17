f188783ec6c5e5978610bfa288a692b38b80fcca
Lydiah-Nyawira <lydiahnyawira4@gmail.com>
2026-08-17 10:49:26 +0300

    Add Client model to clients app

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    CLIENT_TYPE_CHOICES = [
+        ("individual", "Individual"),
+        ("company", "Company"),
+        ("diaspora", "Diaspora"),
+    ]
+
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    client_type = models.CharField(max_length=20, choices=CLIENT_TYPE_CHOICES, default="individual")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name# Create your models here.
