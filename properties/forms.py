83f6bf0246ce116fb4a67f47ca7197b4ef3c722a
Lydiah-Nyawira <lydiahnyawira4@gmail.com>
2026-08-19 14:01:31 +0300

    Add latitude and longitude to PropertyForm

--- properties/forms.py
+++ properties/forms.py
@@ -5,4 +5,4 @@ class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
         fields = ['title_number', 'property_code', 'location', 'county', 'sub_county',
-                   'coordinates', 'land_size', 'property_user', 'property_tenure', 'ownership_type']
+                   'coordinates', 'latitude', 'longitude', 'land_size', 'property_user', 'property_tenure', 'ownership_type']
