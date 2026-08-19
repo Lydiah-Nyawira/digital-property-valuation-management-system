1937966e0966dd6350385dcde97cad447764ee4a
Lydiah-Nyawira <lydiahnyawira4@gmail.com>
2026-08-19 13:52:06 +0300

    Add latitude and longitude fields to Property model

--- properties/models.py
+++ properties/models.py
@@ -26,6 +26,8 @@ class Property(models.Model):
     county = models.CharField(max_length=100)
     sub_county = models.CharField(max_length=100)
     coordinates = models.CharField(max_length=100, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     land_size = models.DecimalField(max_digits=12, decimal_places=4, help_text="Land size in hectares")
     property_user = models.CharField(max_length=100, blank=True, null=True)
     property_tenure = models.CharField(max_length=20, choices=TENURE_CHOICES)
